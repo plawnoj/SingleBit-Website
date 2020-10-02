ba8cbe52eaccd2661413643d84009bc7afd7ce70
jon <43426006+jcwalp@users.noreply.github.com>
2020-10-02 01:14:44 -0400

    Finished Form page
    Only bugs might be checkbox value transfer from js file to php, also might adjust some css files, ready to push to live

---
+++ assets/mail/pc_consult.js
@@ -0,0 +1,107 @@
+$(function () {
+    $(
+        "#contactForm input,#contactForm textarea,#contactForm button"
+    ).jqBootstrapValidation({
+        preventSubmit: true,
+        submitError: function ($form, event, errors) {
+            // additional error messages or events
+        },
+        submitSuccess: function ($form, event) {
+            event.preventDefault(); // prevent default submit behaviour
+            // get values from FORM
+            /*
+            for checkbox vals if doesn't work right away look
+            to assign string values like yes/no in the javascript filte
+            then pass it through to the php file for parsing
+            */
+            var name = $("input#name").val();
+            var email = $("input#email").val();
+            var phone = $("input#phone").val();
+            var budget = $("input#budget").val();
+            var software = $("input#software").val();
+            var monitor = $("input#monitor").val();
+            var keyboard = $("input#keyboard").val();
+            var mouse = $("input#mouse").val();
+            var speakers = $("input#speakers").val();
+            var headset = $("input#headset").val();
+            var microphone = $("input#microphone").val();
+            var firstName = name; // For Success/Failure Message
+            // Check for white space in name for Success/Fail message
+            if (firstName.indexOf(" ") >= 0) {
+                firstName = name.split(" ").slice(0, -1).join(" ");
+            }
+            $this = $("#sendMessageButton");
+            $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
+            $.ajax({
+                url: "/assets/mail/pc_consult.php",
+                type: "POST",
+                data: {
+                    name: name,
+                    phone: phone,
+                    email: email,
+                    budget: budget,
+                    software: software,
+                    monitor: monitor,
+                    keyboard: keyboard,
+                    mouse: mouse,
+                    speakers: speakers,
+                    headset: headset,
+                    microphone: microphone
+                },
+                cache: false,
+                success: function () {
+                    // Success message
+                    $("#success").html("<div class='alert alert-success'>");
+                    $("#success > .alert-success")
+                        .html(
+                            "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
+                        )
+                        .append("</button>");
+                    $("#success > .alert-success").append(
+                        "<strong>Your message has been sent. </strong>"
+                    );
+                    $("#success > .alert-success").append("</div>");
+                    //clear all fields
+                    $("#contactForm").trigger("reset");
+                },
+                error: function () {
+                    // Fail message
+                    $("#success").html("<div class='alert alert-danger'>");
+                    $("#success > .alert-danger")
+                        .html(
+                            "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;"
+                        )
+                        .append("</button>");
+                    $("#success > .alert-danger").append(
+                        $("<strong>").text(
+                            "Sorry " +
+                                firstName +
+                                ", it seems that my mail server is not responding. Please try again later!"
+                        )
+                    );
+                    $("#success > .alert-danger").append("</div>");
+                    //clear all fields
+                    $("#contactForm").trigger("reset");
+                },
+                complete: function () {
+                    setTimeout(function () {
+                        $this.prop("disabled", false); // Re-enable submit button when AJAX call is complete
+                    }, 1000);
+                },
+            });
+        },
+        filter: function () {
+            return $(this).is(":visible");
+        },
+    });
+
+    $('a[data-toggle="tab"]').click(function (e) {
+        e.preventDefault();
+        $(this).tab("show");
+    });
+});
+
+/*When clicking on Full hide fail/success boxes */
+$("#name").focus(function () {
+    $("#success").html("");
+});
--- assets/mail/pc_consult.php
+++ assets/mail/pc_consult.php
@@ -3,7 +3,8 @@
 if(empty($_POST['name'])      ||
    empty($_POST['email'])     ||
    empty($_POST['phone'])     ||
-   empty($_POST['message'])   ||
+   empty($_POST['budget'])   ||
+   empty($_POST['software']) ||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
    echo "No arguments Provided!";
@@ -13,12 +14,19 @@ if(empty($_POST['name'])      ||
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
-$message = strip_tags(htmlspecialchars($_POST['message']));
+$budget = strip_tags(htmlspecialchars($_POST['budget']);
+$software = strip_tags(htmlspecialchars($_POST['software']));
+$monitor = strip_tags(htmlspecialchars($_POST['monitor']));
+$keyboard = strip_tags(htmlspecialchars($_POST['keyboard']));
+$mouse = strip_tags(htmlspecialchars($_POST['mouse']));
+$speakers = strip_tags(htmlspecialchars($_POST['speakers']));
+$headset = strip_tags(htmlspecialchars($_POST['headset']));
+$microphone = strip_tags(htmlspecialchars($_POST['microphone']));
 
 // Create the email and send the message
 $to = 'singlebitsocial@gmail.com'; // Add your email address in between the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nBudget: $budget\n\nSoftware Used: $software\n\nMonitor: $monitor\n\nKeyboard: $keyboard\n\nMouse: $mouse\n\nSpeakers: $speakers\n\nHeadset: $headset\n\nMicrophone: $microphone";
 $headers = "From: noreply@singlebit.dev\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
